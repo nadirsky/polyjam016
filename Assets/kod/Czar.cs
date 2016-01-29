dd7a368832010fcea1631357351834cc8e75028c
pracalic <pawlo168@gmail.com>
2016-01-29 23:07:12 +0100

    PawelFirstCommit

---
+++ Assets/kod/Czar.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Czar : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public virtual void Czaruj()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/kod/MeatStock.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MeatStock : MonoBehaviour {
+
+	public static MeatStock instance = null;
+
+	public Text manaText;
+
+
+	public int meatAdd = 1;
+	public int meatPoints;
+
+	public int secondPlayerMeat;
+
+	// Use this for initialization
+	void Awake () {
+		if (instance == null) 
+		{
+			instance = this;
+			return;
+		}
+	
+		Destroy (this);
+	}
+
+
+	public virtual void DoladujMane()
+	{
+		meatPoints += meatAdd;
+		manaText.text = meatPoints.ToString ();
+	}
+
+	public void DoladujManeDrugiemu()
+	{
+		secondPlayerMeat += meatAdd;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
